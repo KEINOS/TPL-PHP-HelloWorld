47ed7b0d9c6ce0520ef967105f389d3617381578
KEINOS <github@keinos.com>
2020-03-07 12:41:25 +0900

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,6 +4,20 @@
 #  Functions
 # =============================================================================
 
+function buildContainerTest () {
+    echoTitle 'Rebuilding test container'
+    isDockerAvailable || {
+        echoMsg '❌  Docker not installed.'
+        exit 1
+    }
+
+    echo '- Building container ...'
+    docker-compose build --no-cache test || {
+        echoMsg '❌  Fail to build test container.'
+        exit 1
+    }
+}
+
 function echoHR(){
     # Draw Horizontal Line
     printf '%*s\n' "${SCREEN_WIDTH}" '' | tr ' ' ${1-=}
@@ -35,12 +49,22 @@ function getPathScript () {
     echo "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 }
 
+function isDockerAvailable () {
+    docker version 2>/dev/null 1>/dev/null && {
+        return 0
+    }
+
+    return 1
+}
+
 function isInsideContainer () {
     isInsideTravis && {
+        echoMsg '💡  You are running inside TravisCI.'
         return 0
     }
 
     [ -f /.dockerenv ] && {
+        echoMsg '💡  You are running inside Docker container.'
         return 0
     }
 
@@ -53,6 +77,42 @@ function isInsideTravis () {
     }
 
     return 1
+}
+
+function isInstalledPakcage () {
+    [ -f "./vendor/bin/${1}" ] &&{
+        ./vendor/bin/$1 --version 2>/dev/null 1>/dev/null && {
+            return 0
+        }
+    }
+
+    return 1
+}
+
+function isInstalledRequirements () {
+    isInstalledPakcage phpunit && \
+    isInstalledPakcage phan && \
+    isInstalledPakcage php-coveralls && \
+    isInstalledPakcage phpstan && \
+    isInstalledPakcage psalm && {
+        return 0
+    }
+
+    return 1
+}
+
+function removeContainerPrune () {
+    echo '- Removing prune container and images ...'
+    docker container prune -f 1>/dev/null
+    docker image prune -f 1>/dev/null
+}
+
+function runContainerTest () {
+    echoMsg 'Calling test container ...'
+    echoTitle 'Running Tests in Container'
+    docker-compose run test
+
+    return $?
 }
 
 # =============================================================================
@@ -74,43 +134,40 @@ cd "$(getPathParent)"
 # =============================================================================
 #  RUN Tests on local (Recall this script via Docker)
 # =============================================================================
-! isInsideContainer && {
-    [ "${1}" = "build" ] && {
-        echoMsg 'Rebuilding test container ...'
-        docker-compose build --no-cache test || {
-            echoMsg '❌  Fail to build test container.'
-            exit 1
-        }
+[ "${1}" = "build" ] && {
+    buildContainerTest
+}
+
+! [ "${1}" = "local" ] && ! isInsideContainer && {
+    isInstalledRequirements && {
+        echoMsg '💡  Requirements all installed in local'
+        echo '- RECOMMEND: Use "composer test local" command for faster testresults.'
     }
-    echoMsg 'Calling test container ...'
-    echoTitle 'Running Tests in Container'
-    docker-compose run test
+
+    runContainerTest
     result=$?
 
-    echo '- Removing prune container and images ...'
-    docker container prune -f 1>/dev/null
-    docker image prune -f 1>/dev/null
-
+    removeContainerPrune
     exit $result
 }
 
 # =============================================================================
 #  RUN Tests on Container (Actual Tests)
 # =============================================================================
-echoTitle 'Running tests'
-
 # -----------------------------------------------------------------------------
 #  Main
 # -----------------------------------------------------------------------------
 
-# Load Token for COVERALLS
-[ -f ./tests/conf/COVERALLS.env ] && {
-    source ./tests/conf/COVERALLS.env
-    export COVERALLS_RUN_LOCALLY=$COVERALLS_RUN_LOCALLY
-    export COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN
-}
+echoTitle 'Running tests'
 
-echoTitle 'Diagnose: composer'
+isInstalledRequirements || {
+    echoMsg '❌  Missing: composer packages.'
+    echo '- Some packages are missing. Run "composer install"'
+    exit 1
+}
+echo '- Basic requirements of composer installed.'
+
+echoTitle 'DIAGNOSE: composer'
 composer diagnose
 if [ $? -eq 0 ];
     then echoMsg '✅  Diagnose: passed'
@@ -121,7 +178,6 @@ if [ $? -eq 0 ];
 fi
 
 echoTitle 'TEST: PHPUnit'
-
 echo '- Removing old reports ...'
 rm -rf ./report/*
 echo '- Running PHPUnit'
@@ -136,47 +192,6 @@ if [ $? -eq 0 ];
     }
 fi
 
-echoTitle 'TEST: Code Coverage'
-[ "${COVERALLS_REPO_TOKEN:+notfound}" ] && {
-    echo '- Token for COVERALLS found.'
-
-    COVERALLS_RUN_LOCALLY=1
-    dry_run_mode='--dry-run'
-    isInsideTravis && {
-        echo '- Running inside Travis detected.'
-        export TRAVIS=${TRAVIS:-true}
-        export COVERALLS_RUN_LOCALLY=1
-        export CI_NAME=${CI_NAME:-travis-ci}
-        dry_run_mode=
-    }
-    ./vendor/bin/php-coveralls \
-        --config=./tests/conf/coveralls.yml \
-        --json_path=./report/coveralls-upload.json \
-        --verbose \
-        $dry_run_mode \
-        --no-interaction
-    if [ $? -eq 0 ];
-        then {
-            echoMsg '✅  COVERALLS: finished'
-        };
-        else {
-            php -v | grep Xdebug 1>/dev/null 2>/dev/null
-            [ $? -eq 0 ] && {
-                echoMsg '❌  COVERALLS: failed'
-                echo '- ENV'; env | sort
-                echo '- Other variables'; (set -o posix ; set)
-                all_tests_passed=1
-            } || {
-                echoMsg '🛑  COVERALLS: SKIP'
-                echo '- Xdebug extension is not enabled.'
-            }
-        }
-    fi
-} || {
-    echoMsg '🛑  COVERALLS: SKIP'
-    echo '- Token not set for COVERALLS.'
-}
-
 echoTitle 'TEST: PHPStan'
 ./vendor/bin/phpstan analyse src --level=max
 if [ $? -eq 0 ];
@@ -187,7 +202,7 @@ if [ $? -eq 0 ];
     }
 fi
 
-echoTitle 'TEST: PSalm w/fix issue'
+echoTitle 'TEST: PSalm (w/ alter and issue=all option)'
 ./vendor/bin/psalm \
     --root ./src \
     --alter \
@@ -212,6 +227,52 @@ if [ $? -eq 0 ];
         all_tests_passed=1
     }
 fi
+
+echoTitle 'TEST: Code Coverage'
+[ -f ./tests/conf/COVERALLS.env ] && {
+    # Load Token for COVERALLS
+    source ./tests/conf/COVERALLS.env
+    export COVERALLS_RUN_LOCALLY=$COVERALLS_RUN_LOCALLY
+    export COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN
+}
+[ "${COVERALLS_REPO_TOKEN:+notfound}" ] && {
+    echo '- Token for COVERALLS found.'
+
+    export COVERALLS_RUN_LOCALLY=1
+    option_dry_run='--dry-run'
+    isInsideTravis && {
+        echo '- Running inside Travis detected.'
+        export TRAVIS=${TRAVIS:-true}
+        export CI_NAME=${CI_NAME:-travis-ci}
+        option_dry_run=
+    }
+    ./vendor/bin/php-coveralls \
+        --config=./tests/conf/coveralls.yml \
+        --json_path=./report/coveralls-upload.json \
+        --verbose \
+        $option_dry_run \
+        --no-interaction
+    if [ $? -eq 0 ];
+        then {
+            echoMsg '✅  COVERALLS: finished'
+        };
+        else {
+            php -v | grep Xdebug 1>/dev/null 2>/dev/null
+            [ $? -eq 0 ] && {
+                echoMsg '❌  COVERALLS: failed'
+                echo '- ENV'; env | sort
+                echo '- Other variables'; (set -o posix ; set)
+                all_tests_passed=1
+            } || {
+                echoMsg '🛑  COVERALLS: SKIP'
+                echo '- Xdebug extension is not enabled.'
+            }
+        }
+    fi
+} || {
+    echoMsg '🛑  COVERALLS: SKIP'
+    echo '- Token not set for COVERALLS.'
+}
 
 if [ $all_tests_passed -eq 0 ];
     then echoTitle '✅  All tests passed.'
